f2ebdf0492cf7f0a70e7ba2ced4db51d5945cb7d
harshitcodex0 <harshit456987@hotmail.com>
2024-04-21 15:14:51 +0530

    Nike

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Footer from './Footer.jsx';
+import Hero from  './Hero.jsx';
+import SuperQuality from './SuperQuality.jsx';
+import CustomerReviews from './CustomerReviews.jsx';
+import PopularProducts from './PopularProducts.jsx';
+import Subscribe from './Subscribe.jsx';
+import SpecialOffer from './SpecialOffer.jsx';
+import Services from './Services.jsx';
+
+export {
+    Footer,
+    Hero,
+    SuperQuality,
+    CustomerReviews,
+    PopularProducts,
+    Subscribe,
+    SpecialOffer,
+    Services
+}
