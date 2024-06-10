750141ca9361ef40a531da0db79761d00b921e91
harshitcodex0 <harshit456987@hotmail.com>
2024-06-10 11:20:46 +0530

    .

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button"
 import { arrowRight } from "../assets/icons"
+import { statistics } from "../constants"
 
 
 const Hero = () => {
@@ -8,19 +9,27 @@ const Hero = () => {
     id="home"
     className="w-full flex xl:flex-row flex-col min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>The New Arrival</span>
+      <div className="relative xl:w-2/5 flex flex-col justify-center px-6 items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
+        <h1 className="mt-5 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading[82] font-bold ">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br />
-          <span>Nike</span>
-          Shoes
+          <span className=" text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
-        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <p className="font-montserrat mt-6 mb-12 text-slate-gray text-lg leading-8 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
         <Button
         label="Shop Now"
         iconURL={arrowRight}
         />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat)=>(
+            <div key={stat.label}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
+
 
       </div>
 
